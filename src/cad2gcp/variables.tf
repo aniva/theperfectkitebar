140e0b9fba9ff115b9f6633ce6927b71a9d2328a
aniva <ivanovan@gmail.com>
2025-05-04 23:48:54 -0400

    many changes

--- src/cad2gcp/variables.tf
+++ src/cad2gcp/variables.tf
@@ -6,5 +6,15 @@ variable "project_id" {
 variable "region" {
   description = "GCP Region"
   type        = string
-  default     = "us-central1"
+  default     = "us-central1"      # it’s fine to leave non-secret defaults
+}
+
+variable "billing_account_id" {
+  description = "GCP Billing Account ID"
+  type        = string
+}
+
+variable "budget_display_name" {
+  description = "Display name of the Budget"
+  type        = string
 }
